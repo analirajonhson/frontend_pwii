45f9212b11e8a9b44bee529434338ba4ce474426
Ana Vieira <analirajonhson@gmail.com>
2023-04-06 21:59:28 -0300

    adicionando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
+import { HomeComponent } from './components/home/home.component';
+import { CadastarProdutosComponent } from './components/produtos/cadastar-produtos/cadastar-produtos.component';
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+   {path:'produtos', component:ListarProdutosComponent},
+   {path:'produtos/cadastrar', component:CadastarProdutosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
+import { CadastarProdutosComponent } from './components/produtos/cadastar-produtos/cadastar-produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ListarProdutosComponent,
+    CadastarProdutosComponent
   ],
   imports: [
     BrowserModule,
